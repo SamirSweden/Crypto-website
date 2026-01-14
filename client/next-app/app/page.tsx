0f5e4cbdca355ff0b67736c039cdb14e335959d6
Samir Azimov <samirazimov488@gmail.com>
2026-01-14 20:54:09 +0500

    Create page.tsx

---
+++ client/next-app/app/page.tsx
@@ -0,0 +1,24 @@
+import Banner from "@/components/Banner";
+import Title from "@/components/Title";
+import Investor from "@/components/Investor";
+import Learn from "@/components/Learn";
+import Product from "@/components/Products";
+import Footer from "@/components/Footer";
+
+
+
+
+export default function Home() {
+  return (
+    <>
+    <main>
+      <Banner />
+      <Title />
+      <Investor />
+      <Learn />
+      <Product />
+      <Footer />
+    </main>
+    </>
+  );
+}
