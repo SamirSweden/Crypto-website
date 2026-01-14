4f6bd8c84374933e62867e45590b3eac2a9000d0
Samir Azimov <samirazimov488@gmail.com>
2026-01-14 20:47:18 +0500

    Create Learn.tsx

---
+++ client/next-app/app/components/Learn.tsx
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+const Learn = () => {
+    return(
+        <section className="bg-black py-[50px] h-[80vh] px-[90px]">
+            <div className="max-w-[1200px] p-0 mx-auto w-full h-full ">
+                <div className="flex items-center  h-[80%]">
+                    <div className="left__">
+                        <img alt="phone" height={500} width={570} src="/phone.png" ></img>
+                    </div>
+                    <div className="max-w-[446px] w-[446px] ">
+                        <h2 className="text-white text-[45px] font-bold capitalize mb-[20px]">Learn As You Grow</h2>
+                        <p className="text-white text-[18px] font-bold">
+                            Our goal is to make investing in financial markets more affordable, more intuitive, and more fun, no matter how much experience you have (or don’t have).
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+export default Learn
+
+
+
