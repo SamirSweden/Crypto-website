1d1e30bf6d31bc4d6a162795cc025db1bfd4a9c9
Samir Azimov <samirazimov488@gmail.com>
2026-01-14 21:10:20 +0500

    Create site.ts

---
+++ client/next-app/app/config/site.ts
@@ -0,0 +1,38 @@
+//export type SiteConfig = typeof siteConfig;
+export type SiteConfig = typeof siteConfig
+export const siteConfig = {
+  name: "Uzum",
+  description: "Check your website .",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About us",
+      href: "/about",
+    },
+    {
+      label: "News",
+      href: "/news",
+    },
+  ],
+  navMenuItems: [
+   
+   {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About us",
+      href: "/about",
+    },
+    {
+      label: "News",
+      href: "/news",
+    },
+  ],
+  links: {
+    github: "https://github.com/SamirSweden",
+  },
+};
