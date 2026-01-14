5bdee03c9f321336e425044e38f86695bc1814ca
Samir Azimov <samirazimov488@gmail.com>
2026-01-14 20:52:22 +0500

    Create tailwind.config.js

---
+++ client/next-app/app/tailwind.config.js
@@ -0,0 +1,33 @@
+import {heroui} from "@heroui/theme"
+import { keyframes } from "framer-motion";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(10px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        }
+      }
+    },
+    animation : {
+      fadeIn : 'fadeIn 0.4s ease-out forwards'
+    }
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+}
+
+module.exports = config;
