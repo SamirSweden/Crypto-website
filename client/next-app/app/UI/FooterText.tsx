0c3c70e5ad7a1a7fd4c1743aed8175b1c6129a62
Samir Azimov <samirazimov488@gmail.com>
2026-01-14 21:09:15 +0500

    Create FooterText.tsx

---
+++ client/next-app/app/UI/FooterText.tsx
@@ -0,0 +1,28 @@
+
+
+
+const FooterText = () => {
+    return (
+        <div className="">
+            <span className="mb-[24px] text-white ">@2025 Robinhood. All rights reserved.</span>
+            <div className="flex items-center flex-col">
+                <p className="text-gray-300 text-sm mb-[22px]">
+                    Robinhood means Robinhood Markets and its in-application and web experiences with its family of wholly owned subsidiaries which includes Robinhood Financial, Robinhood Securities, and Robinhood Crypto.
+                </p>
+                <p className="text-gray-300 text-sm mb-[24px] text-left">
+                    All investments involve risks, including the possible loss of capital.
+                </p>
+                <div>
+                    <p className="text-gray-400 mb-[32px] text-sm">Securities trading is offered to self-directed customers by Robinhood Financial. Robinhood Financial is a member of the</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+export default FooterText
+
+
