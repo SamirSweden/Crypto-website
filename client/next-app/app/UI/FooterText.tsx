bc351370aa6d2f2140762aba587d814eb8c05cc3
Samir Azimov <samirazimov488@gmail.com>
2026-01-22 01:38:51 +0500

    Update FooterText.tsx

--- client/next-app/app/UI/FooterText.tsx
+++ client/next-app/app/UI/FooterText.tsx
@@ -5,15 +5,15 @@ const FooterText = () => {
     return (
         <div className="">
             <span className="mb-[24px] text-white ">@2025 Robinhood. All rights reserved.</span>
-            <div className="flex items-center flex-col">
-                <p className="text-gray-300 text-sm mb-[22px]">
+            <div className="flex items-left flex-col">
+                <p className="text-gray-300 text-sm mt-[22px]!">
                     Robinhood means Robinhood Markets and its in-application and web experiences with its family of wholly owned subsidiaries which includes Robinhood Financial, Robinhood Securities, and Robinhood Crypto.
                 </p>
-                <p className="text-gray-300 text-sm mb-[24px] text-left">
+                <p className="text-gray-300 text-sm m-[24px] mr-0 ml-0 text-left">
                     All investments involve risks, including the possible loss of capital.
                 </p>
                 <div>
-                    <p className="text-gray-400 mb-[32px] text-sm">Securities trading is offered to self-directed customers by Robinhood Financial. Robinhood Financial is a member of the</p>
+                    <p className="text-gray-400 text-left mb-[32px] text-sm">Securities trading is offered to self-directed customers by Robinhood Financial. Robinhood Financial is a member of the</p>
                 </div>
             </div>
         </div>
